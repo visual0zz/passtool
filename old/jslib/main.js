077e3d1508d08b1c37835f617a64f9de8d05ab72
书台 <123456@fake.com>
2022-12-09 00:19:51 +0800

    清理旧的无用代码

--- old/jslib/main.js
+++
@@ -1,7 +0,0 @@
-function include(path){ 
-	var a=document.createElement("script");
-	a.type = "text/javascript"; 
-	a.src=path; 
-	var head=document.getElementsByTagName("head")[0];
-	head.appendChild(a);
-	}
