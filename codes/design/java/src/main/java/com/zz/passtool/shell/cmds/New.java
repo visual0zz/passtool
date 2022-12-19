face1aa9431eb675146a98ee43415892e689fe65
书台 <123456@fake.com>
2022-12-19 13:45:14 +0800

    fix\\

--- codes/design/java/src/main/java/com/zz/passtool/shell/cmds/New.java
+++ codes/design/java/src/main/java/com/zz/passtool/shell/cmds/New.java
@@ -11,7 +11,7 @@ public class New extends Command {
     @Override
     public Object run(String... args) throws Exception {
         ParamCheckUtil.assertTrue(args.length==1,"只能输入一个参数");
-        ParamCheckUtil.assertTrue(args[0].matches("[^\\.!@#$%^&*(){};:'\"?<>,`~]*"),"文件名不能出现特殊符号");
+        ParamCheckUtil.assertTrue(args[0].matches("[^\\.!@#$%^&*(){};:'\"?<>,`~\\\\]*"),"文件名不能出现特殊符号");
         FileCacheService.newFile(new File(FolderService.currentFolder,args[0]+".json"));
         return null;
     }
